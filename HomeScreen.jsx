33d73e78a255c23e909c32f245fcba193c6c23a7
Abram Motaung <st10449144@imconnect.edu.za>
2024-10-25 14:53:08 +0200

    @ App

--- HomeScreen.jsx
+++ HomeScreen.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { Ionicons } from './expo/vector-icons';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('./logo.png')} style={{ width: 100, height: 100 }} />
+      <Image source={require('./assets/empowering-the-nation-high-resolution-logo (1).png')} style={{ width: 100, height: 100 }} />
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Empowering the Nation</Text>
       <TouchableOpacity onPress={() => navigation.navigate('SixMonthCourses')}>
         <Text style={{ fontSize: 18, color: 'blue' }}>Six-Month Courses</Text>
